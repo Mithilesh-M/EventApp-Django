3c65c31d0cde00e2a19832cc6cd103da100ac19c
Mithilesh-M <msmithilesh@yahoo.in>
2021-04-12 16:21:49 +0530

    Add event list view in the app
    Added event list in the application

--- eventapp/views.py
+++ eventapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event, Timing
 
-# Create your views here.
+class EventListView(generic.ListView):
+    model = Event
+    paginate_by = 10
